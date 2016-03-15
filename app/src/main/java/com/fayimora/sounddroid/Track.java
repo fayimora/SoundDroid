6fc79cdb8ff4662faad5e676c48af60a65a878ed
Fayimora Femi-Balogun <fayi@fayimora.com>
2016-03-15 19:42:19 +0000

    Add initial track domain

---
+++ app/src/main/java/com/fayimora/sounddroid/Track.java
@@ -0,0 +1,34 @@
+package com.fayimora.sounddroid;
+
+/**
+ * Created by fayimora on 15/03/2016.
+ */
+public class Track {
+    private int id;
+    private String title;
+    private String streamUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStreamUrl() {
+        return streamUrl;
+    }
+
+    public void setStreamUrl(String streamUrl) {
+        this.streamUrl = streamUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
